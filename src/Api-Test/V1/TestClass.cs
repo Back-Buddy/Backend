c993b288f41bace323190e24233b846bfcfb0010
PlaySkyHD <nic14markfort@gmail.com>
2025-04-01 19:34:44 +0200

    feat: Initial commit

---
+++ src/Api-Test/V1/TestClass.cs
@@ -0,0 +1,13 @@
+﻿namespace BackBuddy.Api.Test.V1
+{
+    [TestClass]
+    public class TestClass
+    {
+
+        [TestMethod]
+        public void TestMethod()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
---
+++ src/Integration-Test/V1/TestClass.cs
@@ -0,0 +1,13 @@
+﻿namespace BackBuddy.Integration_Test.V1
+{
+    [TestClass]
+    public class TestClass
+    {
+
+        [TestMethod]
+        public void TestMethod()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
