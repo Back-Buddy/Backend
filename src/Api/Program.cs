cf89eff2f3073fdc96d05929d6a51b2ca4a137d2
PlaySkyHD <nic14markfort@gmail.com>
2025-04-01 20:39:04 +0200

    feat: Add Swagger

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,18 +1,15 @@
+using BackBuddy.Api.Service.Swagger;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+builder.Services.ConfigureFullSwaggerConfig();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
@@ -20,4 +17,4 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-app.Run();
+await app.RunAsync();
---
+++ src/Api/Swagger/SwaggerConfigurationExtension.cs
@@ -0,0 +1,73 @@
+﻿using Asp.Versioning;
+using Asp.Versioning.Conventions;
+using Microsoft.OpenApi.Models;
+
+namespace BackBuddy.Api.Service.Swagger
+{
+    public static class SwaggerConfigurationExtension
+    {
+        public static void ConfigureFullSwaggerConfig(this IServiceCollection col)
+        {
+            col.AddApiVersioning(
+                options =>
+                {
+                    options.DefaultApiVersion = new ApiVersion(1.0);
+                    options.AssumeDefaultVersionWhenUnspecified = true;
+                    options.ReportApiVersions = true;
+                    options.ApiVersionReader = ApiVersionReader.Combine(
+                           new UrlSegmentApiVersionReader(),
+                           new QueryStringApiVersionReader("api-version"),
+                           new HeaderApiVersionReader("X-Version"),
+                           new MediaTypeApiVersionReader("x-version"));
+                })
+            .AddMvc(options => options.Conventions.Add(new VersionByNamespaceConvention()))
+            .AddApiExplorer(setup =>
+            {
+                setup.GroupNameFormat = "'v'VVV";
+                setup.SubstituteApiVersionInUrl = true;
+            });
+
+            col.AddEndpointsApiExplorer();
+
+            col.AddSwaggerGen(option =>
+            {
+                // Add OpenAPI document with valid version
+                option.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "BackBuddy API",
+                    Version = "v1",
+                    Description = "BackBuddy API",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "BackBuddy",
+                    }
+                });
+
+                option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Please enter a valid token",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    BearerFormat = "JWT",
+                    Scheme = "Bearer"
+                });
+
+                option.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type=ReferenceType.SecurityScheme,
+                                Id="Bearer"
+                            }
+                        },
+                        Array.Empty<string>()
+                    }
+                });
+            });
+        }
+    }
+}
