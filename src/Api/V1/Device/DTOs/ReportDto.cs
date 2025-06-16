86f44ecf4e533dba3e9eb66ce0dad8224e650925
PlaySkyHD <nic14markfort@gmail.com>
2025-06-16 20:46:41 +0200

    fix: Apply Comments

--- src/Api/V1/Device/DTOs/ReportDto.cs
+++ src/Api/V1/Device/DTOs/ReportDto.cs
@@ -6,11 +6,11 @@ namespace BackBuddy.Api.Service.V1.Device.DTOs
     {
         public required Guid Id { get; init; }
         public required string Name { get; init; }
-        public required ReportVisibilityType? VisibilityType { get; init; } // Only set if the report is getted by the user who created it
-        public required Guid? DeviceId { get; init; } // Only set if the report is getted by the user who created it
+        public required ReportVisibilityType? VisibilityType { get; init; } // Only set if the report is retrieved by the user who created it
+        public required Guid? DeviceId { get; init; } // Only set if the report is retrieved by the user who created it
         public required DateTime StartTime { get; init; }
         public required DateTime EndTime { get; init; }
-        public required List<Guid>? UsedLogsIds { get; set; } // Only set if the report is getted by the user who created it
+        public required List<Guid>? UsedLogsIds { get; set; } // Only set if the report is retrieved by the user who created it
         public required List<DeviceLogDto>? UsedLogs { get; init; }
         public required ReportMetadataDto Metadata { get; init; }
         public required DateTime CreatedAt { get; init; }
